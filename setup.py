90db370869728a18c38e6a9a4d8a4ec5fb4ee011
piakoch <123562360+piakoch@users.noreply.github.com>
2023-06-02 11:55:37 +0200

    setup config

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
 setuptools.setup(
-    name = "My_Snake_lib",
+    name = "The-ultimate-Snake",
     version = "0.0.1",
     author = "pia",
     author_email = "pia.koch@student.uva.nl",
     python_requires = ">=3.6",
-    packages = setuptools.find_packages(),
-    entry_points={"console_scripts": ["play= My_Snake_lib.game:main"]}
+    packages = ['My_Snake_lib'],
+    entry_points={"console_scripts": ["play= The-ultimate-Snake.My_Snake_lib.game:main"]}
 )
