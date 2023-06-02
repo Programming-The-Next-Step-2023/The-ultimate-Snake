b9ca1d34e55e40b5273b5a3eaaa03d6f35842e31
piakoch <123562360+piakoch@users.noreply.github.com>
2023-06-02 11:29:50 +0200

    setup entry_points

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setuptools.setup(
     author = "pia",
     author_email = "pia.koch@student.uva.nl",
     python_requires = ">=3.6",
-    packages = setuptools.find_packages()
+    packages = setuptools.find_packages(),
+    entry_points={"console_scripts": ["play= My_Snake_lib.game:main"]}
 )
